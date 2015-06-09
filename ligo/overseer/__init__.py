1d5ecc6402a6738d544438cfbfe9fbdfa9ac70b9
Branson Stephens <stephenb@uwm.edu>
2015-06-09 17:06:05 -0500

    Fixed typo with packaging.

--- ligo/overseer/__init__.py
+++ ligo/overseer/__init__.py
@@ -14,6 +14,6 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with LIGO.ORG.  If not, see <http://www.gnu.org/licenses/>.
+# along with lvalert-overseer.  If not, see <http://www.gnu.org/licenses/>.
 
 
