176043e07b912eb9425197c0d42a2dc00076c436
Branson Stephens <stephenb@uwm.edu>
2015-10-02 15:54:11 -0500

    Changed version number.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = "0.1"
+version = "0.1.1"
 AUTHOR = 'Branson Stephens'
 AUTHOR_EMAIL = 'branson.stephens@ligo.org'
 LICENSE = 'GPLv3'
